feadc5d12b7039d8ba01a40ce202d6f51ff06818
Jordan Zeeb <jmzeeb@gmail.com>
2020-05-16 16:37:44 -0500

    finished hw4

---
+++ HW4/HW4.X/i2c_master_noint.h
@@ -0,0 +1,23 @@
+#ifndef I2C_MASTER_NOINT_H__
+#define I2C_MASTER_NOINT_H__
+// Header file for i2c_master_noint.c
+// helps implement use I2C1 as a master without using interrupts
+
+#include <xc.h>
+
+void i2c_master_setup(void); // set up I2C1 as master
+void i2c_master_start(void); // send a START signal
+void i2c_master_restart(void); // send a RESTART signal
+void i2c_master_send(unsigned char byte); // send a byte (either an address or data)
+unsigned char i2c_master_recv(void); // receive a byte of data
+void i2c_master_ack(int val); // send an ACK (0) or NACK (1)
+void i2c_master_stop(void); // send a stop
+
+// Custom functions
+void i2c_set_pin(unsigned char write_address, unsigned char regist, unsigned char value);
+  // Write a pin value to a specific register
+
+unsigned char i2c_read_pin(unsigned char read_address, unsigned char regist);
+  // Read a value from a specific register
+
+#endif
